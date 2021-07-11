f429ef14f7a5d70272544e58004e11f7304ab2d0
DenerThiago21 <denerthi.ibi@gmail.com>
2021-07-11 14:19:10 -0300

    Styled Components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
-import axios from 'axios';
+import React from "react";
+import Routes from './routes';
+import './style.css';
 
-function App(props) {
-  const [ usuario, setUsuario ] = useState('');
-  
-  function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`)
-         .then((response) => {
-           console.log(response.data);
-         });
-  }
-  
+function App() {
+    
   return (
-    <>
-      <input className="usuarioInput" value={usuario} placeholder="Usuario" onChange={(e) => {setUsuario(e.target.value)}} /> 
-      <button type="button" onClick={handlePesquisa}>Pesquisar</button>
-    </>
+      <Routes />
   );
 }
 
