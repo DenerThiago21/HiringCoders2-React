e7d446eea6bbe87fa681566d7f56c30b37c93687
DenerThiago21 <denerthi.ibi@gmail.com>
2021-07-11 14:22:41 -0300

    Styled Components adicionado ao projeto

---
+++ src/pages/Repositories/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import * as S from './styled';
+
+function Repositories() {
+    return (
+        <S.Container>
+            <S.Title>Repositories</S.Title>
+            <S.List>
+                <S.ListItem>Repositório: nome do Repo1</S.ListItem>
+                <S.ListItem>Repositório: nome do Repo2</S.ListItem>
+                <S.ListItem>Repositório: nome do Repo3</S.ListItem>
+                <S.ListItem>Repositório: nome do Repo4</S.ListItem>
+                <S.ListItem>Repositório: nome do Repo5</S.ListItem>
+                <S.ListItem>Repositório: nome do Repo6</S.ListItem>
+                <S.ListItem>Repositório: nome do Repo7</S.ListItem>
+                <S.ListItem>Repositório: nome do Repo8</S.ListItem>
+                <S.ListItem>Repositório: nome do Repo9</S.ListItem>
+                <S.ListItem>Repositório: nome do Repo10</S.ListItem>
+            </S.List>
+        </S.Container>
+        
+    );
+}
+
+export default Repositories;
